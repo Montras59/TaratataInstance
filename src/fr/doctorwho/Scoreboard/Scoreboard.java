4913b1669456294b542534daa85f9bff1dbc4d81
Antoine ADAM <flancraft10@gmail.com>
2017-12-06 12:23:34 +0100

    Update Scoreboard.java

--- src/fr/doctorwho/Scoreboard/Scoreboard.java
+++ src/fr/doctorwho/Scoreboard/Scoreboard.java
@@ -11,34 +11,41 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Scoreboard {
 	private Plugin pl = Main.getInstance();
+	//stockage des scoreboards de tout les joueur
 	private static HashMap<Player,ScoreboardSign> boards;
+	//constructeur que je n'ai pas utilisé
 	public Scoreboard(){
 	}
+	//function pour ajouté tout les joueur sur le serveur au scoreboard
 	public void start(){
 		boards = new HashMap<>();
 		for(Player p : Bukkit.getOnlinePlayers()){
 			PlayerScoreboard(p);
 		}
 	}
+	//function pour créer ou update le scoreboard d'un joueur
 	public void PlayerScoreboard(Player p){
 		if(!boards.containsKey(p)){
 			createPlayerScoreboard(p);
 		}
 		updatePlayerScoreboard(p);
 	}
+	//function pour détruire tout les scoreboards
 	public void stop(){
 		for(Entry<Player, ScoreboardSign> p : boards.entrySet()){
 			p.getValue().destroy();
 		}
 		boards.clear();
 	}
+	//function pour détruire le scoreboard du joueur ciblé
 	public void Disconnecte(Player p){
 		if(boards.containsKey(p)){
 			boards.get(p).destroy();
 			boards.remove(p);
 		}
 	}
-	public void updatePlayerScoreboard(Player p){
+	//function interne update le scoreboard du joueur ciblé
+	private void updatePlayerScoreboard(Player p){
 		ScoreboardSign sc = boards.get(p);
 		HashMap<Integer,String> lignes = new HashMap<>();
 		lignes.put(0,"§a");
@@ -47,34 +54,32 @@ public class Scoreboard {
 			sc.setLine(ligne.getKey(), ligne.getValue());
 		}
 	}
-	public boolean createPlayerScoreboard(Player p){
-		if(!boards.containsKey(p)){
-			ScoreboardSign scoreboard = new ScoreboardSign(p, ChatColor.AQUA+"§lDoctorWhoRP");
-			scoreboard.create();
-			String[] lignes = {
-					"§a",
-					ChatColor.YELLOW+"§l§nInformation:",
-					"§b",
-					ChatColor.RED+"§lGrade: "+ChatColor.GRAY+"-------",
-					ChatColor.RED+"§lNiveau: "+ChatColor.AQUA+"--",
-					ChatColor.RED+"§lProgression: "+ChatColor.AQUA+"--%",
-					ChatColor.RED+"§lServeur: "+ChatColor.YELLOW+"------",
-					"§c",
-					ChatColor.BLUE+"§lQuête (--):",
-					ChatColor.DARK_AQUA+"§l---------",
-					ChatColor.BLUE+"§lEtape (--/--):",
-					ChatColor.GRAY+"§l---------",
-					ChatColor.GOLD+"§lIncarnation: "+ChatColor.BLUE+"---------",
-					"§d",
-					ChatColor.GREEN+"§lDoctorWhoRP.fr"};
-			int i=0;
-			for(String l : lignes){
-				scoreboard.setLine(i, l);
-				i++;
-			}
-			boards.put(p, scoreboard);
-			return true;
+	//function interne crée le scoreboard du joueur ciblé
+	private void createPlayerScoreboard(Player p){
+		//ScoreboardSign est une api 1.11 que j'ai mi à jour
+		ScoreboardSign scoreboard = new ScoreboardSign(p, ChatColor.AQUA+"§lDoctorWhoRP");
+		scoreboard.create();
+		String[] lignes = {
+				"§a",
+				ChatColor.YELLOW+"§l§nInformation:",
+				"§b",
+				ChatColor.RED+"§lGrade: "+ChatColor.GRAY+"-------",
+				ChatColor.RED+"§lNiveau: "+ChatColor.AQUA+"--",
+				ChatColor.RED+"§lProgression: "+ChatColor.AQUA+"--%",
+				ChatColor.RED+"§lServeur: "+ChatColor.YELLOW+"------",
+				"§c",
+				ChatColor.BLUE+"§lQuête (--):",
+				ChatColor.DARK_AQUA+"§l---------",
+				ChatColor.BLUE+"§lEtape (--/--):",
+				ChatColor.GRAY+"§l---------",
+				ChatColor.GOLD+"§lIncarnation: "+ChatColor.BLUE+"---------",
+				"§d",
+				ChatColor.GREEN+"§lDoctorWhoRP.fr"};
+		int i=0;
+		for(String l : lignes){
+			scoreboard.setLine(i, l);
+			i++;
 		}
-		return false;
+		boards.put(p, scoreboard);
 	}
 }
